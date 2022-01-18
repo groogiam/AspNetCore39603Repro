b8911a34a7828c35e572f7a3fd62d321138ab83a
Greg Horvath <horvath.105@gmail.com>
2022-01-18 12:29:12 -0500

    Initial commit.

---
+++ Repro39603/Client/JsInteropService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace Repro39603.Client
+{
+    public class JsInteropService
+    {
+        public JsInteropService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        #region Properties
+
+        private readonly IJSRuntime _jsRuntime;
+
+        #endregion
+
+        public ValueTask DownloadFromStream(string fileName, Stream data)
+        {
+            //using causes exception
+            using var streamRef = new DotNetStreamReference(data);
+            return _jsRuntime.InvokeVoidAsync("downloadFileFromStream", fileName ?? string.Empty, streamRef);
+        }
+    }
+}
